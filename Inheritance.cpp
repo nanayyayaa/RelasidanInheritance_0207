e4ff9f42f3155faa371342a13bcf0f87e7b7d273
nanayyayaa <jiejueejuju@gmail.com>
2026-05-11 19:33:50 +0700

    commit 17

---
+++ Inheritance.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//pembuatan class orang
+class orang {
+public:
+    //deklarasi member variabel dari class orang
+    string nama;
+
+    //pembuatan constructor dan destructor dari class orang
+    orang(string pNama) :nama(pNama) {
+        cout << "Orang dibuat\n" << endl;
+    }
+}
