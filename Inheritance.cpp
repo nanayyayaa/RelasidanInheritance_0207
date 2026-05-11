3e588feac1794814189b101fb802160b324fefe8
nanayyayaa <jiejueejuju@gmail.com>
2026-05-11 19:37:14 +0700

    commit 18

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -12,4 +12,20 @@ public:
     orang(string pNama) :nama(pNama) {
         cout << "Orang dibuat\n" << endl;
     }
+
+    ~orang() {
+        cout << "Orang dihapus\n" << endl;
+    }
+
+    //pembuatan function jumlah
+    int jumlah(int a, int b) {
+        return a + b;
+    }
+
+};
+
+//pembuatan class pelajar sebagai pewaris dari class orang
+class pelajar : public orang {
+public:
+    //deklarasi member variabel dari class pelajar
 }
