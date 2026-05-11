6de44a1f14b6494f5ad786d7e8da17b83e98a22f
nanayyayaa <jiejueejuju@gmail.com>
2026-05-11 18:09:38 +0700

    commit 7

---
+++ Asosiasi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//deklarasi class dokter
+class dokter;
+
+//deklarasi dan pembuatan class pasien
+class pasien {
+public:
+    //deklarasi member variabel dari class pasien
+    string nama;
+    vector<dokter*> daftar_dokter;
+
+    //pembuatan constructor dan destructor dari class pasien
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    };
+}
