2349acbf65e190f2e1866a6538bbf76283ef8b06
nanayyayaa <jiejueejuju@gmail.com>
2026-05-11 18:41:53 +0700

    commit 13

--- Asosiasi.cpp
+++ Asosiasi.cpp
@@ -96,4 +96,13 @@ int main()
     varDokter2->cetakPasien();
     varPasien1->cetakDokter();
     varPasien2->cetakDokter();
+
+    //mendelete variabel - variabel pointer dari memori
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+
+    return 0;
+    
 }
